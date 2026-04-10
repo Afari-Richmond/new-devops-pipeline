b5f81fba67d65a1335869e1aa748fbae727c6644
richmondafari <afaririchmond1000@gmail.com>
2026-04-10 14:49:01 +0000

    (feat): Add CI Pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello DevOps! This is a simple Node.js application.');
+console.log('I am Learning CI/CD using Github Actions ');
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Starting Tests');
+setTimeout(() => console.log("...waiting for 3secs"), 3000);
+
+console.log("Tets Complete");
+
